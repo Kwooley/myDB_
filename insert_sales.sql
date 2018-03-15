6185fa0a2f3bff8b063d3e44e97c0caf8b30756c
kwool <kwool@KAYMacPro15.local>
2018-03-15 22:53:38 +0900

    kmac 15

--- insert_sales.sql
+++ insert_sales.sql
@@ -1,20 +1,22 @@
 delete from Sales;
 
-Insert into Sales values ( 2017, 7, 1, 1,  "콜라", 90, 269250)
-Insert into Sales values ( 2017, 7, 1, 2, "사이다", 104, 309000);
-Insert into Sales values ( 2017, 7, 2, 3, "샘플러5종", 267, 3997336);
-Insert into Sales values ( 2017, 7, 2, 4, "생활밀착", 856, 5554360);
-Insert into Sales values ( 2017, 7, 2, 5, "놀라운에일", 326, 2112681);
-Insert into Sales values ( 2017, 7, 2, 6, "레드락IPA", 98, 637000);
-Insert into Sales values ( 2017, 7, 2, 7, "레드락라거", 3098, 12389956);
-Insert into Sales values ( 2017, 7, 2, 8, "모카스타우트", 360, 2516423);
-Insert into Sales values ( 2017, 7, 2, 9, "슬로우IPA", 80, 560000);
-Insert into Sales values ( 2017, 7, 2, 10, "강남페일에일", 482, 3133000);
-Insert into Sales values ( 2017, 7, 2, 11, "걸작IPA", 464, 3244423);
-Insert into Sales values ( 2017, 7, 2, 12, "일상탈출", 0, 0);
-Insert into Sales values ( 2017, 7, 2, 13, "설레임", 0, 0);
-Insert into Sales values ( 2017, 7, 2, 14, "하와이안골든에일", 0, 0);
+-- JUL 2107, CAT 1,2
+Insert into Sales values ( 2017, 7, 1, 1,  "콜라", 		90, 269250);
+Insert into Sales values ( 2017, 7, 1, 2, "사이다", 		104, 309000);
+Insert into Sales values ( 2017, 7, 2, 3, "샘플러5종", 		267, 3997336);
+Insert into Sales values ( 2017, 7, 2, 4, "생활밀착", 		856, 5554360);
+Insert into Sales values ( 2017, 7, 2, 5, "놀라운에일", 		326, 2112681);
+Insert into Sales values ( 2017, 7, 2, 6, "레드락IPA", 		98, 637000);
+Insert into Sales values ( 2017, 7, 2, 7, "레드락라거", 		3098, 12389956);
+Insert into Sales values ( 2017, 7, 2, 8, "모카스타우트", 		360, 2516423);
+Insert into Sales values ( 2017, 7, 2, 9, "슬로우IPA", 		80, 560000);
+Insert into Sales values ( 2017, 7, 2, 10, "강남페일에일", 		482, 3133000);
+Insert into Sales values ( 2017, 7, 2, 11, "걸작IPA", 		464, 3244423);
+Insert into Sales values ( 2017, 7, 2, 12, "일상탈출", 		0, 0);
+Insert into Sales values ( 2017, 7, 2, 13, "설레임", 		0, 0);
+Insert into Sales values ( 2017, 7, 2, 14, "하와이안골든에일", 		0, 0);
 
+-- JUL 2107, CAT 3
 Insert into Sales values ( 2017, 7, 3, 101, "뻘건감자",	 		185, 1127194);
 Insert into Sales values ( 2017, 7, 3, 102, "촉촉오징어땅콩",	144, 1440000);
 Insert into Sales values ( 2017, 7, 3, 103, "골빔면",	 	110, 981000);
@@ -32,14 +34,64 @@ Insert into Sales values ( 2017, 7, 4, 114, "앵그리버드RED",	 	0, 0);
 Insert into Sales values ( 2017, 7, 4, 115, "앵그리버드RED(반반)",	 	0, 0);
 Insert into Sales values ( 2017, 7, 3, 116, "봉다리복만두",	 	0, 0);
 
+-- JUL 2107, CAT 4
 
-Insert into Sales values ( 2017, 7, 2, 201, "CAN 강남", 	4, 22000);
-Insert into Sales values ( 2017, 7, 2, 202, "CAN 진짜500", 	7, 18900);
-Insert into Sales values ( 2017, 7, 2, 203, "CAN 놀라운", 	3, 16500);
-Insert into Sales values ( 2017, 7, 2, 204, "CAN 생활밀착",	7, 34300);
-Insert into Sales values ( 2017, 7, 2, 205, "CAN 레드락IPA",	2, 11000);
-Insert into Sales values ( 2017, 7, 2, 206, "CAN 모카스타우트",	7, 38500);
-Insert into Sales values ( 2017, 7, 2, 207, "CAN 걸작IPA",	1, 6000);
-Insert into Sales values ( 2017, 7, 2, 208, "CAN 일상탈출",	0, 0);
-Insert into Sales values ( 2017, 7, 2, 209, "CAN 설레임",	0, 0);
-Insert into Sales values ( 2017, 7, 2, 210, "CAN 하와이안골든에일",	0, 0);
+Insert into Sales values ( 2017, 7, 4, 201, "CAN 강남", 	4, 22000);
+Insert into Sales values ( 2017, 7, 4, 202, "CAN 진짜500", 	7, 18900);
+Insert into Sales values ( 2017, 7, 4, 203, "CAN 놀라운", 	3, 16500);
+Insert into Sales values ( 2017, 7, 4, 204, "CAN 생활밀착",	7, 34300);
+Insert into Sales values ( 2017, 7, 4, 205, "CAN 레드락IPA",	2, 11000);
+Insert into Sales values ( 2017, 7, 4, 206, "CAN 모카스타우트",	7, 38500);
+Insert into Sales values ( 2017, 7, 4, 207, "CAN 걸작IPA",	1, 6000);
+Insert into Sales values ( 2017, 7, 4, 208, "CAN 일상탈출",	0, 0);
+Insert into Sales values ( 2017, 7, 4, 209, "CAN 설레임",	0, 0);
+Insert into Sales values ( 2017, 7, 4, 210, "CAN 하와이안골든에일",	0, 0);
+
+-- AUG. 2017, CAT 1,2
+
+Insert into Sales values ( 2017, 8, 1, 1,  "콜라", 		75, 225000);
+Insert into Sales values ( 2017, 8, 1, 2, "사이다", 		114, 339000);
+Insert into Sales values ( 2017, 8, 2, 3, "샘플러5종", 		273, 4087500);
+Insert into Sales values ( 2017, 8, 2, 4, "생활밀착", 		843, 5476250);
+Insert into Sales values ( 2017, 8, 2, 5, "놀라운에일", 	299, 1942200);
+Insert into Sales values ( 2017, 8, 2, 6, "레드락IPA", 		92, 598000);
+Insert into Sales values ( 2017, 8, 2, 7, "레드락라거", 	2745, 10968800);
+Insert into Sales values ( 2017, 8, 2, 8, "모카스타우트", 	368, 2571800);
+Insert into Sales values ( 2017, 8, 2, 9, "슬로우IPA", 		82, 574000);
+Insert into Sales values ( 2017, 8, 2, 10, "강남페일에일", 	522, 3388450);
+Insert into Sales values ( 2017, 8, 2, 11, "걸작IPA", 		351, 2454200);
+Insert into Sales values ( 2017, 8, 2, 12, "일상탈출", 		0, 0);
+Insert into Sales values ( 2017, 8, 2, 13, "설레임", 		0, 0);
+Insert into Sales values ( 2017, 8, 2, 14, "하와이안골든에일", 	0, 0);
+
+-- AUG. 2017, CAT 3 
+Insert into Sales values ( 2017, 8, 3, 101, "뻘건감자",	 	173, 1120000);
+Insert into Sales values ( 2017, 8, 3, 102, "촉촉오징어땅콩",	137, 1370000);
+Insert into Sales values ( 2017, 8, 3, 103, "골빔면",	 	107, 963000);
+Insert into Sales values ( 2017, 8, 3, 104, "허연감자",	 	230, 1398500);
+Insert into Sales values ( 2017, 8, 3, 105, "스팸튀김",	 	63, 567000);
+Insert into Sales values ( 2017, 8, 4, 106, "앵그리버드",	280, 4456000);
+Insert into Sales values ( 2017, 8, 5, 107, "크리스피텐더",	152, 2269500);
+Insert into Sales values ( 2017, 8, 3, 108, "버팔로윙",	 	69,  1014000);
+Insert into Sales values ( 2017, 8, 3, 109, "나초칩",	 	144, 1152000);
+Insert into Sales values ( 2017, 8, 3, 110, "그린라이트사라다",	 64, 1014000);
+Insert into Sales values ( 2017, 8, 3, 111, "소시지와친구들", 	 96, 1235000);
+Insert into Sales values ( 2017, 8, 3, 112, "맥앤치즈샐러드",	 38, 494000);
+Insert into Sales values ( 2017, 8, 3, 113, "백화점육포",	 60, 660000);
+Insert into Sales values ( 2017, 8, 4, 114, "앵그리버드RED",	 	0, 0);
+Insert into Sales values ( 2017, 8, 4, 115, "앵그리버드RED(반반)",	 	0, 0);
+Insert into Sales values ( 2017, 8, 3, 116, "봉다리복만두",	 	0, 0);
+
+-- AUG. 2017, CAT 4 
+Insert into Sales values ( 2017, 8, 4, 201, "CAN 강남", 	8, 44000);
+Insert into Sales values ( 2017, 8, 4, 202, "CAN 진짜500", 	5, 13500);
+Insert into Sales values ( 2017, 8, 4, 203, "CAN 놀라운", 	5, 27500);
+Insert into Sales values ( 2017, 8, 4, 204, "CAN 생활밀착",	20, 98000);
+Insert into Sales values ( 2017, 8, 4, 205, "CAN 레드락IPA",	1, 5500);
+Insert into Sales values ( 2017, 8, 4, 206, "CAN 모카스타우트",	15, 82500);
+Insert into Sales values ( 2017, 8, 4, 207, "CAN 걸작IPA",	9, 54000);
+Insert into Sales values ( 2017, 8, 4, 208, "CAN 일상탈출",	0, 0);
+Insert into Sales values ( 2017, 8, 4, 209, "CAN 설레임",	0, 0);
+Insert into Sales values ( 2017, 8, 4, 210, "CAN 하와이안골든에일",	0, 0);
+
+
